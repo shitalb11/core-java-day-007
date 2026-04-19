a23e3144f59a9b462230d2300936fd421fc64dcf
Shital Bhosale <shitalb075@gmail.com>
2026-04-19 20:09:30 +0530

    5

--- src/main/java/org/example/java8features/C.java
+++ src/main/java/org/example/java8features/C.java
@@ -1,4 +1,17 @@
 package org.example.java8features;
 
 public class C {
+    public static void main(String[] args) {
+        class A{}
+        class B extends A{}
+        A a=new B();
+        A a2=new A(){};
+
+        interface MessageService{
+            public abstract void send(String message);
+        }
+        class WhatsApp implements MessageService{
+            public void send()
+        }
+    }
 }
