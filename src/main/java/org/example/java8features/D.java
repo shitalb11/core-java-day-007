7a2e6b278e3fcfcb1fc29c4ff8815b6df527fd3b
Shital Bhosale <shitalb075@gmail.com>
2026-04-20 22:43:32 +0530

    new

--- src/main/java/org/example/java8features/D.java
+++ src/main/java/org/example/java8features/D.java
@@ -1,5 +1,15 @@
 package org.example.java8features;
 
 public class D {
-
+    public static void main(String[] args) {
+        abstract class UserService{
+            public abstract void sayHello();
+        }
+        class UserServiceImpl extends UserService{
+            @Override
+            public void sayHello(){
+                System.out.println("hello world");
+            }
+        }
+    }
 }
