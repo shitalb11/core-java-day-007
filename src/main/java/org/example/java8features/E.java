ca13c9fc3345baf06dae91cc664a7714ed21732d
Shital Bhosale <shitalb075@gmail.com>
2026-04-21 16:17:39 +0530

    new

--- src/main/java/org/example/java8features/E.java
+++ src/main/java/org/example/java8features/E.java
@@ -1,5 +1,13 @@
 package org.example.java8features;
 
 public class E {
-
+    public static void main(String[] args) {
+        interface UserService{
+            public static void count(){
+                System.out.println("counting");
+            }
+        }
+        //static method will be called using interface name
+        UserService.count();
+    }
 }
