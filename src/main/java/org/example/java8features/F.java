0eaed1e84750bf0259d8155b038c11ddd383f32d
Shital Bhosale <shitalb075@gmail.com>
2026-04-22 21:38:39 +0530

    new

--- src/main/java/org/example/java8features/F.java
+++ src/main/java/org/example/java8features/F.java
@@ -1,16 +1,5 @@
 package org.example.java8features;
 
 public class F {
-    public static void main(String[] args) {
-        @FunctionalInterface
-        interface Runnable{
-            void run();
 
-            default void display(){
-                System.out.println("display - Functional Interface");
-            }
-        }
-
-
-    }
 }
---
+++ src/main/java/org/example/java8features/I.java
@@ -0,0 +1,16 @@
+package org.example.java8features;
+
+public class I {
+    public static void main(String[] args) {
+        @FunctionalInterface
+        interface Runnable{
+            void run();
+
+            default void display(){
+                System.out.println("display - Functional Interface");
+            }
+        }
+
+
+    }
+}
