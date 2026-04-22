d5b66f5a0a3b2e4f467911e9cd4e281a587a6ef5
Shital Bhosale <shitalb075@gmail.com>
2026-04-22 21:41:32 +0530

    new

--- src/main/java/org/example/java8features/F.java
+++ src/main/java/org/example/java8features/F.java
@@ -1,5 +1,15 @@
 package org.example.java8features;
 
 public class F {
+    public static void main(String[] args) {
 
+        interface UserService{
+            void login();
+
+            default void logout(){
+                System.out.println("You are logged out. default implementation for all subclass");
+            }
+        }
+
+    }
 }
