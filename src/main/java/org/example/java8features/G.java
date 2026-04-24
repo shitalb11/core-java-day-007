b74d832be0712dd164583c2dc30775633633f2c7
Shital Bhosale <shitalb075@gmail.com>
2026-04-24 19:20:57 +0530

    new

--- src/main/java/org/example/java8features/G.java
+++ src/main/java/org/example/java8features/G.java
@@ -1,5 +1,14 @@
 package org.example.java8features;
 
 public class G {
+    public static void main(String[] args) {
+        interface Interface1{
+            void method1();
 
+            default void display(){
+                System.out.println("Interface1.display");
+            }
+        }
+
+    }
 }
