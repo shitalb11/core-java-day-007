7da8a169c65bea902d8d38164b19fea5e88b9bc2
Shital Bhosale <shitalb075@gmail.com>
2026-04-24 19:25:22 +0530

    new

--- src/main/java/org/example/java8features/G.java
+++ src/main/java/org/example/java8features/G.java
@@ -4,11 +4,28 @@ public class G {
     public static void main(String[] args) {
         interface Interface1{
             void method1();
-
             default void display(){
                 System.out.println("Interface1.display");
             }
         }
+
+        interface Interface2{
+            void method2();
+            default void display(){
+                System.out.println("Interface2.display");
+            }
+        }
+
+        class A implements Interface1, Interface2{
+
+            @Override
+            public void method1(){
+                System.out.println("Method 1 overridden in A");
+            }
+
+
+        }
+
 
     }
 }
