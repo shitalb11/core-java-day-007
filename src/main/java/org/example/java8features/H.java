a90974781ae535c7bb5a5fe6d3d82b1ee5d19767
Shital Bhosale <shitalb075@gmail.com>
2026-04-25 11:45:16 +0530

    new

--- src/main/java/org/example/java8features/H.java
+++ src/main/java/org/example/java8features/H.java
@@ -1,5 +1,11 @@
 package org.example.java8features;
 
 public class H {
+    public static void main(String[] args) {
 
+        @FunctionalInterface
+            interface Interface1{
+            void m1();
+        }
+    }
 }
