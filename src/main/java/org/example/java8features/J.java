095ee525f3bcc00cf60a5b625dfde712c94c5ea6
Shital Bhosale <shitalb075@gmail.com>
2026-04-28 17:37:25 +0530

    new

--- src/main/java/org/example/java8features/J.java
+++ src/main/java/org/example/java8features/J.java
@@ -1,5 +1,20 @@
 package org.example.java8features;
 
 public class J {
+    public static void main(String[] args) {
 
+        interface A{
+            void m1();
+        }
+        A a = () -> System.out.println("m1");
+        a.m1();
+
+        interface B {
+            void m2(int a);
+        }
+        B b = x -> System.out.println("m2" + x);
+        b.m2(10);
+
+
+    }
 }
