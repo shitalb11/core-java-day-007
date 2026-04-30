f2cb62a2c06b82d012ea4a131d8a810fbfbbdb2e
Shital Bhosale <shitalb075@gmail.com>
2026-04-30 21:42:26 +0530

    new

--- src/main/java/org/example/java8features/K.java
+++ src/main/java/org/example/java8features/K.java
@@ -2,6 +2,13 @@ package org.example.java8features;
 
 public class K {
     public static void main(String[] args) {
+        interface NumberProcessor {
+            int process(int number);
+        }
+        NumberProcessor square = (int number) -> {
+            return number * number;
+        };
+
 
     }
 }
