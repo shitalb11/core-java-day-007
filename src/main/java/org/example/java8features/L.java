c4c5528e0e59d202e50ba31a2c3a148bef8f1a68
Shital Bhosale <shitalb075@gmail.com>
2026-04-30 22:18:16 +0530

    new

--- src/main/java/org/example/java8features/L.java
+++ src/main/java/org/example/java8features/L.java
@@ -1,5 +1,12 @@
 package org.example.java8features;
 
 public class L {
-
+    public static void main(String[] args) {
+        interface Predicate{
+            boolean test(int input);
+        }
+        Predicate isEven = input -> input % 2 == 0;
+        System.out.println("is even test : " + isEven.test(10));
+        System.out.println("is even test : " + isEven.test(11));
+    }
 }
