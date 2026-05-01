bf7a5c87d8a43b8e03c4aaf38bf3b82ad8bd4120
Shital Bhosale <shitalb075@gmail.com>
2026-05-01 18:11:54 +0530

    new

--- src/main/java/org/example/java8features/M.java
+++ src/main/java/org/example/java8features/M.java
@@ -1,5 +1,16 @@
 package org.example.java8features;
 
 public class M {
+    public static void main(String[] args) {
 
+        interface Predicate<X> {
+            boolean test(X input);
+        }
+        Predicate<Integer> isEven = (Integer input) -> input % 2 == 0;
+        System.out.println("is Even test : " + isEven.test(10));
+
+        Predicate<String> startWithA = (String str) -> str.startsWith("A");
+        System.out.println(startWithA.test("Abc"));
+        System.out.println(startWithA.test("xyz"));
+    }
 }
