a09909dc648f26e4dc092f13f29ac12e9a61fe23
Shital Bhosale <shitalb075@gmail.com>
2026-05-02 22:58:51 +0530

    new

--- src/main/java/org/example/java8features/N.java
+++ src/main/java/org/example/java8features/N.java
@@ -2,6 +2,12 @@ package org.example.java8features;
 
 public class N {
     public static void main(String[] args) {
+        interface Predicate<X, Y> {
+           boolean test(X x , Y y);
+        }
 
+        Predicate<String, String> stringStringPredicate = (s1 , s2) -> s1.equalsIgnoreCase(s2);
+        System.out.println(stringStringPredicate.test("abc" , "abc"));
+        System.out.println(stringStringPredicate.test("zyx" , "abc"));
     }
 }
