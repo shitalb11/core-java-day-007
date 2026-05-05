1bb4c6831a4239be420d1b07b2c2d45efbd57bc9
Shital Bhosale <shitalb075@gmail.com>
2026-05-05 22:18:37 +0530

    new

--- src/main/java/org/example/java8features/O.java
+++ src/main/java/org/example/java8features/O.java
@@ -2,6 +2,14 @@ package org.example.java8features;
 
 public class O {
     public static void main(String[] args) {
+        interface Predicate<X> {
+            boolean test(X input);
+        }
+
+        Predicate<Integer> isEven = (Integer input) -> input % 2 == 0;
+        System.out.println("is even test : " + isEven.test(10));
+
+
 
     }
 }
