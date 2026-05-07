6191a22b5c8c1efc21f23adc109bac96bf7cafa5
Shital Bhosale <shitalb075@gmail.com>
2026-05-07 23:25:10 +0530

    new

--- src/main/java/org/example/java8features/R.java
+++ src/main/java/org/example/java8features/R.java
@@ -1,7 +1,10 @@
 package org.example.java8features;
 
+import java.util.function.Consumer;
+
 public class R {
     public static void main(String[] args) {
-
+        Consumer<String> consumer = str -> System.out.println("Entered string is " + str);
+        consumer.accept("hello world");
     }
 }
