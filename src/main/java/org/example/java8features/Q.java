dd9027ef16ce168e9f27931c57ed6115b78cf0b2
Shital Bhosale <shitalb075@gmail.com>
2026-05-07 19:29:08 +0530

    neww

--- src/main/java/org/example/java8features/Q.java
+++ src/main/java/org/example/java8features/Q.java
@@ -1,7 +1,12 @@
 package org.example.java8features;
 
+import java.util.function.Function;
+
 public class Q {
     public static void main(String[] args) {
-
+            Function<String, Integer> function = (String str) -> {
+                return str.length();
+            };
+            System.out.println("String length is "+function.apply("IT Shaala"));
     }
 }
