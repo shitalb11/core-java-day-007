51d4bbd210ba02f60b13bc38ca281f05dfa8c9a3
Shital Bhosale <shitalb075@gmail.com>
2026-05-08 23:27:26 +0530

    new

--- src/main/java/org/example/java8features/S.java
+++ src/main/java/org/example/java8features/S.java
@@ -1,7 +1,10 @@
 package org.example.java8features;
 
+import java.util.function.BiFunction;
+
 public class S {
     public static void main(String[] args) {
-
+        BiFunction<String, String, Integer> biFunction = (s1, s2) -> s1.length() + s2.length();
+        System.out.println(biFunction.apply("hi", "hello"));
     }
 }
