7e76cfe753df956a28fce0efea9e9f0b7fc97c64
Shital Bhosale <shitalb075@gmail.com>
2026-05-09 11:47:12 +0530

    new

--- src/main/java/org/example/java8features/T.java
+++ src/main/java/org/example/java8features/T.java
@@ -6,7 +6,7 @@ public class T {
                   int add(int number1, int number2);
             }
             //1.traditional way
-        class NumberProcessorImpl implements NumberProcessor{
+            class NumberProcessorImpl implements NumberProcessor{
                 @Override
             public int add(int number1, int number2){
                     return number1 + number2;
@@ -14,6 +14,17 @@ public class T {
             }
             NumberProcessor numberProcessor = new NumberProcessorImpl();
             System.out.println("traditional way add is = " + numberProcessor.add(100,200));
+
+
+            //2.anonymous way
+            NumberProcessor anonymous = new NumberProcessor() {
+                @Override
+                public int add(int number1, int number2) {
+                    return number1+number2;
+                }
+            };
+            System.out.println("anonymous way add is = " + anonymous.add(100, 200));
+
 
     }
 }
