599d86b65fae6834cf8861c29bc0e0ade345f00e
Shital Bhosale <shitalb075@gmail.com>
2026-05-09 12:09:02 +0530

    new

--- src/main/java/org/example/java8features/T.java
+++ src/main/java/org/example/java8features/T.java
@@ -26,5 +26,14 @@ public class T {
             System.out.println("anonymous way add is = " + anonymous.add(100, 200));
 
 
+            //3.lambda expression way
+            NumberProcessor ref = T::sumOfTwoNumbers;
+            System.out.println("Method reference way =" + ref.add(100,200));
+
+
+    }
+
+    private static int sumOfTwoNumbers(int number1, int number2) {
+        return number1+number2;
     }
 }
