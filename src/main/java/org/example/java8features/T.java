1663aaf33d140966ad8b352f04588357e01cd81e
Shital Bhosale <shitalb075@gmail.com>
2026-05-09 11:40:50 +0530

    new

--- src/main/java/org/example/java8features/T.java
+++ src/main/java/org/example/java8features/T.java
@@ -2,6 +2,18 @@ package org.example.java8features;
 
 public class T {
     public static void main(String[] args) {
+            interface NumberProcessor{
+                  int add(int number1, int number2);
+            }
+            //1.traditional way
+        class NumberProcessorImpl implements NumberProcessor{
+                @Override
+            public int add(int number1, int number2){
+                    return number1 + number2;
+                }
+            }
+            NumberProcessor numberProcessor = new NumberProcessorImpl();
+            System.out.println("traditional way add is = " + numberProcessor.add(100,200));
 
     }
 }
