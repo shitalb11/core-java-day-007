7b357700f28c86229ef720c89e05dec9f6421e1b
Shital Bhosale <shitalb075@gmail.com>
2026-05-10 19:23:38 +0530

    new

--- src/main/java/org/example/java8features/U.java
+++ src/main/java/org/example/java8features/U.java
@@ -1,7 +1,19 @@
 package org.example.java8features;
 
 public class U {
+
+            U(){}
+
+            U(String str){
+            System.out.println(str);
+            }
+
+            void m1(String str){
+            System.out.println(str);
+        }
+
     public static void main(String[] args) {
 
     }
-}
+
+    }
