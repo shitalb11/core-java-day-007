ce3a0cb5e68daac3523320df39dadf0310a12647
Shital Bhosale <shitalb075@gmail.com>
2026-05-10 19:27:49 +0530

    new

--- src/main/java/org/example/java8features/U.java
+++ src/main/java/org/example/java8features/U.java
@@ -13,7 +13,20 @@ public class U {
         }
 
     public static void main(String[] args) {
+                interface A{
+                    void m1(String str);
+                }
+                A a = str -> System.out.println(str);
+                a.m1("g.m.");
 
-    }
+                A ref = System.out::println;
+                ref.m1("g.a.");
 
+                A ref2 = U::new;
+                ref2.m1("string using constructor reference");
+
+                U u = new U();
+                A ref3 = u::m1;
+                ref3.m1("non static method reference");
     }
+}
