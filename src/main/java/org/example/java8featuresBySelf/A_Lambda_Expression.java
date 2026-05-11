0ec84ebe5a61b981feb36974a3b39e15f726ca06
Shital Bhosale <shitalb075@gmail.com>
2026-05-11 19:52:45 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/A_Lambda_Expression.java
+++ src/main/java/org/example/java8featuresBySelf/A_Lambda_Expression.java
@@ -1,5 +1,12 @@
 package org.example.java8featuresBySelf;
 
-public class A_Lambda_Expression {
+interface Demo{
+    void show();
+}
 
+public class A_Lambda_Expression {
+    public static void main(String[] args) {
+        Demo d = () -> System.out.println("Hello JAVA 8");
+        d.show();
+    }
 }
