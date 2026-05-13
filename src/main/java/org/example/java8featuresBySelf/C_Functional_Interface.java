51ae3b4ac90a6007c60bb6e49e1bd3549a7bee6b
Shital Bhosale <shitalb075@gmail.com>
2026-05-13 19:52:47 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/C_Functional_Interface.java
+++ src/main/java/org/example/java8featuresBySelf/C_Functional_Interface.java
@@ -1,5 +1,15 @@
 package org.example.java8featuresBySelf;
 
+
 public class C_Functional_Interface {
 
+@FunctionalInterface
+interface Square{
+     int square(int x);
+ }
+
+    public static void main(String[] args) {
+        Square s = x -> x*x;
+        System.out.println(s.square(5));
+    }
 }
