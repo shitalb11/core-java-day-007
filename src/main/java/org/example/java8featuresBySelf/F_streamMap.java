4b7fa8348571f4eb857e5d8a5f551ed3094ab31a
Shital Bhosale <shitalb075@gmail.com>
2026-05-14 10:37:38 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/F_streamMap.java
+++ src/main/java/org/example/java8featuresBySelf/F_streamMap.java
@@ -1,7 +1,14 @@
 package org.example.java8featuresBySelf;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class F_streamMap {
     public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5);
 
+        nums.stream()
+                .map(x->x*x)
+                .forEach(System.out::println);
     }
 }
