8f93d226b0b0316fbeb6b758f1d0e216ad509033
Shital Bhosale <shitalb075@gmail.com>
2026-05-15 23:28:03 +0530

    neW

--- src/main/java/org/example/java8featuresBySelf/H_Stream_distinct.java
+++ src/main/java/org/example/java8featuresBySelf/H_Stream_distinct.java
@@ -1,4 +1,14 @@
 package org.example.java8featuresBySelf;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class H_Stream_distinct {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,2,3,3,4,5);
+
+        nums.stream()
+                .distinct()
+                .forEach(System.out::println);
+    }
 }
