86c59fa847ad25435fe67f3ea6cae1003a02e2a8
Shital Bhosale <shitalb075@gmail.com>
2026-05-16 13:00:44 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/J_Method_Reference.java
+++ src/main/java/org/example/java8featuresBySelf/J_Method_Reference.java
@@ -1,4 +1,11 @@
 package org.example.java8featuresBySelf;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class J_Method_Reference {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("A","B","C");
+        names.forEach(System.out::println);
+    }
 }
