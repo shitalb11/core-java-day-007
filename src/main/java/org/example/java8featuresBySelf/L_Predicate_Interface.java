a8ac0b13571f19651d7ec82b18b7999c373209eb
Shital Bhosale <shitalb075@gmail.com>
2026-05-17 09:51:30 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/L_Predicate_Interface.java
+++ src/main/java/org/example/java8featuresBySelf/L_Predicate_Interface.java
@@ -1,7 +1,11 @@
 package org.example.java8featuresBySelf;
 
+import java.util.function.Predicate;
+
 public class L_Predicate_Interface {
     public static void main(String[] args) {
-
+        Predicate<Integer> p = x ->x%2==0;
+        System.out.println(p.test(11));
+        System.out.println(p.test(10));
     }
 }
