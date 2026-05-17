62e9e9b14eb2ba190c285769c2c575c49d7306de
Shital Bhosale <shitalb075@gmail.com>
2026-05-17 09:48:31 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/K_Optional_Class.java
+++ src/main/java/org/example/java8featuresBySelf/K_Optional_Class.java
@@ -1,5 +1,14 @@
 package org.example.java8featuresBySelf;
 
-public class K_Optional_Class {
+import java.sql.SQLOutput;
+import java.util.Optional;
 
+public class K_Optional_Class {
+    public static void main(String[] args) {
+        String name = null;
+
+        Optional<String> obj = Optional.ofNullable(name);
+
+        System.out.println(obj.orElse("Default Name"));
+    }
 }
