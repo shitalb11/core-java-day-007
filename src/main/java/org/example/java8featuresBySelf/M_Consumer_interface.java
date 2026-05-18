332d59f1427cd5c8ca5920f7b5bb50299e2f0cfa
Shital Bhosale <shitalb075@gmail.com>
2026-05-18 19:52:44 +0530

    neW

--- src/main/java/org/example/java8featuresBySelf/M_Consumer_interface.java
+++ src/main/java/org/example/java8featuresBySelf/M_Consumer_interface.java
@@ -1,4 +1,10 @@
 package org.example.java8featuresBySelf;
 
+import java.util.function.Consumer;
+
 public class M_Consumer_interface {
+    public static void main(String[] args) {
+        Consumer<String> c=x-> System.out.println(x);
+        c.accept("Java 8");
+    }
 }
