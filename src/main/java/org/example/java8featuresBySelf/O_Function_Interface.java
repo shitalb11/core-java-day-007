3885e0e9cbd22208e14684f070864ba3e004ef4c
Shital Bhosale <shitalb075@gmail.com>
2026-05-19 21:21:57 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/O_Function_Interface.java
+++ src/main/java/org/example/java8featuresBySelf/O_Function_Interface.java
@@ -1,4 +1,11 @@
 package org.example.java8featuresBySelf;
 
+import java.util.function.Function;
+
 public class O_Function_Interface {
+    public static void main(String[] args) {
+        Function<Integer, Integer> f=x->x*x*x;
+
+        System.out.println(f.apply(3));
+    }
 }
