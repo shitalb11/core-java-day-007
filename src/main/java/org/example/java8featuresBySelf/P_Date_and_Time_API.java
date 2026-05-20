a53aa80afd1d9cd7438417790be053f4aff97116
Shital Bhosale <shitalb075@gmail.com>
2026-05-20 23:41:31 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/P_Date_and_Time_API.java
+++ src/main/java/org/example/java8featuresBySelf/P_Date_and_Time_API.java
@@ -1,9 +1,11 @@
 package org.example.java8featuresBySelf;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 
 public class P_Date_and_Time_API {
     public static void main(String[] args) {
         System.out.println(LocalDate.now());
+        System.out.println(LocalTime.now());
     }
 }
