2f16a797c9c5099cf19e9fdd569671e900ecf893
Shital Bhosale <shitalb075@gmail.com>
2026-05-21 23:47:17 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/Q_Parallel_Stream.java
+++ src/main/java/org/example/java8featuresBySelf/Q_Parallel_Stream.java
@@ -1,4 +1,13 @@
 package org.example.java8featuresBySelf;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Q_Parallel_Stream {
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        nums.parallelStream()
+                .forEach(System.out::println);
+    }
 }
