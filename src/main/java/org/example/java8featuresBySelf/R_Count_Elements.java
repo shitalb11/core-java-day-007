c3b764adc262dd08dec0902f70091899174cf248
Shital Bhosale <shitalb075@gmail.com>
2026-05-25 20:41:29 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/R_Count_Elements.java
+++ src/main/java/org/example/java8featuresBySelf/R_Count_Elements.java
@@ -7,6 +7,10 @@ public class R_Count_Elements {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Ram", "Ravi", "Sham", "Rohan");
 
+        long count = names.stream()
+                .filter(x -> x.startsWith("R"))
+                .count();
 
+        System.out.println(count);
     }
 }
