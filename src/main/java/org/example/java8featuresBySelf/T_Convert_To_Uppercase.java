d39c733dff0d8efbc0393d38eda86eb2269cef4b
Shital Bhosale <shitalb075@gmail.com>
2026-05-27 08:39:44 +0530

    new

--- src/main/java/org/example/java8featuresBySelf/T_Convert_To_Uppercase.java
+++ src/main/java/org/example/java8featuresBySelf/T_Convert_To_Uppercase.java
@@ -1,7 +1,15 @@
 package org.example.java8featuresBySelf;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class T_Convert_To_Uppercase {
     public static void main(String[] args) {
 
+        List<String> names = Arrays.asList("ram", "sham", "sita");
+
+        names.stream()
+                .map(String::toUpperCase)
+                .forEach(System.out::println);
     }
 }
